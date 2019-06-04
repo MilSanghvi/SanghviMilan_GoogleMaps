69626802e80322e9e5dabc571dc008930aba99b4
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-06-04 10:57:42 -0700

    Google Maps 6/4
    Birthplace, Button, Blue marker

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -1,5 +1,9 @@
 package com.example.mymapsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,6 +13,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.util.Log;
+import android.view.View;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,9 +45,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in San Diego and move the camera
+        LatLng sandiego = new LatLng(32.7157, -117.1611);
+        mMap.addMarker(new MarkerOptions().position(sandiego).title("Born in San Diego"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sandiego));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+    }
+    public void changeView(View view) {
+        Log.d("MyMapsApp", "changeView: View button clicked");
+        if (mMap.getMapType() == 1) {
+            mMap.setMapType(2);
+        } else if (mMap.getMapType() == 2) {
+            mMap.setMapType(1);
+        }
     }
 }
